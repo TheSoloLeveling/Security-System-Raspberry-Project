497643e09eae0a2a79557b07cf0f02765ceff140
Bouziane Yassine <bouzianeyassine112@gmail.com>
2022-12-12 23:08:48 +0100

    crud

--- app/back/main.py
+++ app/back/main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restx import Api, Resource
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
 from flask_cors import CORS
 from config import DevConfig
 from models import User, Participant
@@ -27,6 +27,49 @@ participant_model = api.model(
 class Resource(Resource):
     def get(self):
             return {"message":"Hello Word"}
+
+@api.route('/participants')
+class PartcipantsResource(Resource):
+
+    @api.marshal_list_with(participant_model)
+    def get(self):
+        p = Participant.query.all()
+        return p
+
+    @api.marshal_list_with(participant_model)
+    def post(self):
+        data = request.get_json()
+
+        p = Participant(
+            title = data.get('title'),
+            description = data.get('description')
+        )
+
+        p.save()
+
+        return p, 201
+
+@api.route('/participant/<int:id>')
+class PartcipantResource(Resource):
+
+    @api.marshal_list_with(participant_model)
+    def get(self, id):
+        p = Participant.query.get_or_404(id)
+
+        return p
+
+    def put(self, id):
+        
+        p_to_update = Participant.query.get_or_404(id)
+
+        data = request.get_json()
+
+    def delete(self, id):
+        pass
+    def post(self):
+        pass
+    
+        
 
 @app.shell_context_processor
 def make_shell_context():
